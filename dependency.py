7a71b78c83b746651004cd597a8e43117a74368f
Jannis Baum <jannis.baum@gmail.com>
2022-11-07 08:16:36 +0100

    feat: build dependency tag tree from DependencyGraph

--- dependency.py
+++ dependency.py
@@ -1,6 +1,24 @@
+from itertools import chain
 import os
 from nltk.parse.corenlp import CoreNLPDependencyParser, CoreNLPServer, DependencyGraph
-# from nltk.parse.dependencygraph import dot2img
+from nltk.tree.tree import Tree
+
+def dep_tree(graph: DependencyGraph):
+    def _tree(address: int):
+        node = graph.get_by_address(address)
+        word = node['ctag']
+        deps = sorted(chain.from_iterable(node['deps'].values()))
+        if deps:
+            return Tree(word, [_tree(dep) for dep in deps])
+        else:
+            return word
+
+    node = graph.root
+    if not node: return None
+
+    tag = node['ctag']
+    deps = sorted(chain.from_iterable(node['deps'].values()))
+    return Tree(tag, [_tree(dep) for dep in deps])
 
 stanford_dir = os.path.join(
     os.path.dirname(os.path.realpath(__file__)),
@@ -15,8 +33,6 @@ jars = (
 with CoreNLPServer(*jars):
     parser = CoreNLPDependencyParser()
     dep_graph: DependencyGraph = next(parser.raw_parse('Use StanfordParser to parse multiple sentences'))
-
-    # dot_string = dep_graph.to_dot()
-    # dot2img(dot_string, t='png')
-
-    dep_graph.tree().pretty_print()
+    tree = dep_tree(dep_graph)
+    if tree:
+        tree.pretty_print()
