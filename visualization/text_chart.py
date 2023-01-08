1981b24c2edc54e4acb85a0b8fc7f0e63ba7322c
Jannis Baum <jannis.baum@gmail.com>
2023-01-08 13:14:21 +0100

    feat(#15): basic chart

---
+++ visualization/text_chart.py
@@ -0,0 +1,16 @@
+# pyright: reportWildcardImportFromLibrary=false
+from dominate.tags import *
+
+def stacked_bar(data: list[tuple[str, list[float]]], colors: list[str], labels: list[str]) -> str:
+    stack_height = max(sum(values) for _, values in data)
+
+    chart = div()
+    for sentence, values in data:
+        with chart:
+            with div(style='display: flex'):
+                p(sentence, style='flex: 70%')
+                with div(style='flex: 30%; display: flex'):
+                    for n, v in enumerate(values):
+                        span(style=f'flex: 0 1 auto; width: {100 * v / stack_height}%; background-color: {colors[n]}')
+
+    return chart.render()
