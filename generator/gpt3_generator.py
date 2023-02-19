c63df55f760ad1335fe654d314c74aefd21b05c1
Lucas Liebe <lucasliebe.ll@gmail.com>
2023-02-19 22:57:31 +0100

    feat(#14): generate longer gpt3 texts

--- generator/gpt3_generator.py
+++ generator/gpt3_generator.py
@@ -4,5 +4,5 @@ def generate_gpt3_news_from_original(doc):
     openai.organization = OPENAI_ORGA
     openai.api_key = GPT_KEY
     doc += "\n\nWrite a news article about this topic."
-    response = openai.Completion.create(model="text-davinci-003", prompt=doc, max_tokens=500, temperature=0.4)
+    response = openai.Completion.create(model="text-davinci-003", prompt=doc, max_tokens=800, temperature=0.4)
     return response.choices[0].text
