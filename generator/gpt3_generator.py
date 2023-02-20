aaf8625f336b8279d359dd61922eba37e4075288
Lucas Liebe <lucasliebe.ll@gmail.com>
2023-02-20 12:14:23 +0100

    feat(#14): remove error handling on openai requests

--- generator/gpt3_generator.py
+++ generator/gpt3_generator.py
@@ -6,9 +6,6 @@ def generate_gpt3_news_from_original(doc):
     openai.organization = OPENAI_ORGA
     openai.api_key = GPT_KEY
     doc += "\n\nWrite a news article about this topic."
-    try:
-        response = openai.Completion.create(model="text-davinci-003", prompt=doc, max_tokens=800, temperature=0.4)
-        return response.choices[0].text
-    finally:
-        print("Invalid OpenAI Request, Skipping News")
-        return None
+    #long docs can throw error but try catch will not work because of long response time, need to find other solution
+    response = openai.Completion.create(model="text-davinci-003", prompt=doc, max_tokens=800, temperature=0.4)
+    return response.choices[0].text
