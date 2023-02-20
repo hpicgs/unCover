b9fa3fdb22f01b7bde84a4fe6f8065e135165714
Lucas Liebe <lucasliebe.ll@gmail.com>
2023-02-20 14:20:29 +0100

    feat(#14): add max doc length to prevent openai errors

--- generator/gpt3_generator.py
+++ generator/gpt3_generator.py
@@ -6,6 +6,8 @@ def generate_gpt3_news_from_original(doc):
     openai.organization = OPENAI_ORGA
     openai.api_key = GPT_KEY
     doc += "\n\nWrite a news article about this topic."
-    #long docs can throw error but try catch will not work because of long response time, need to find other solution
-    response = openai.Completion.create(model="text-davinci-003", prompt=doc, max_tokens=800, temperature=0.4)
-    return response.choices[0].text
+    response = None
+    # long docs can throw error but try finally will not work because of long response time so we limit len(doc)
+    if len(doc) < 15000:
+        response = openai.Completion.create(model="text-davinci-003", prompt=doc, max_tokens=800, temperature=0.4)
+    return response.choices[0].text if response else None
