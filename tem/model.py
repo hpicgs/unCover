0f8b9d8db17a39f7f9593fd9dc6459696ddeb4cc
Jannis Baum <jannis.baum@gmail.com>
2023-02-28 10:45:38 -0500

    feat(#18): period rank nodes

--- tem/model.py
+++ tem/model.py
@@ -1,3 +1,5 @@
+import graphviz
+
 class TopicEvolution:
     class Topic:
         def __init__(self, data):
@@ -15,3 +17,18 @@ class TopicEvolution:
         self.periods = [
             TopicEvolution.Period(period)
         for period in data]
+
+    def graph(self) -> graphviz.Digraph:
+        g = graphviz.Digraph()
+        g.attr(rankdir='TB')
+
+        # create rank node for each period
+        with g.subgraph() as s:
+            s.attr('node', shape='box')
+            s.attr('edge', style='invis')
+            for n, _ in enumerate(self.periods):
+                s.node(str(n), label=f'Period {n}')
+                if n < len(self.periods) - 1:
+                    s.edge(str(n), str(n + 1))
+
+        return g
