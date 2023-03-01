45737c71064c70366ef5302883b63ade12892afd
Jannis Baum <jannis.baum@gmail.com>
2023-03-01 08:08:40 -0500

    feat(#20): pass args to tem

--- tem/process.py
+++ tem/process.py
@@ -3,11 +3,27 @@ import yaml
 
 from tem.model import TopicEvolution
 
-def get_topic_evolution(text: str) -> TopicEvolution:
-    p = subprocess.Popen(
-        'tem/topic-evolution-model/.build.out/out',
-        stdin=subprocess.PIPE,
-        stdout=subprocess.PIPE,
-        stderr=subprocess.PIPE)
+def get_topic_evolution(
+    text: str,
+    c: float,
+    alpha: float,
+    beta: float,
+    gamma: float,
+    delta: float,
+    theta: int,
+    mergeThreshold: float,
+    evolutionThreshold: float,
+) -> TopicEvolution:
+    p = subprocess.Popen([
+            'tem/topic-evolution-model/.build.out/out',
+            '--c', str(c),
+            '--alpha', str(alpha),
+            '--beta', str(beta),
+            '--gamma', str(gamma),
+            '--delta', str(delta),
+            '--theta', str(theta),
+            '--mergeThreshold', str(mergeThreshold),
+            '--evolutionThreshold',str(evolutionThreshold) 
+        ], stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
     out = p.communicate(input=text.encode())[0].decode()
     return TopicEvolution(yaml.safe_load(out))
