fcca560133935618e508853a5fa8651d17589ca3
Lucas Liebe <lucasliebe.ll@gmail.com>
2023-03-02 13:52:10 +0100

    feat(#23): update python version to 3.10

--- stylometry/showcase/dep_colors.py
+++ stylometry/showcase/dep_colors.py
@@ -3,7 +3,6 @@ from itertools import chain
 
 from nltk.parse.dependencygraph import DependencyGraph
 from nltk.tree.tree import Tree
-from typing import Tuple, Dict
 
 
 def index_tree(graph: DependencyGraph):
@@ -22,7 +21,7 @@ def index_tree(graph: DependencyGraph):
     deps = sorted(chain.from_iterable(node['deps'].values()))
     return Tree(str(node['address']), [_tree(dep) for dep in deps])
 
-def color_string(text: str, hsv: Tuple[float, float, float], hl: bool=False):
+def color_string(text: str, hsv: tuple[float, float, float], hl: bool=False):
     rgb = colorsys.hsv_to_rgb(*hsv)
     rgb = [int(c * 255) for c in rgb]
     return ''.join([
@@ -36,11 +35,11 @@ def print_dep_colors(dep_graph: DependencyGraph):
     if not tree: return
 
     root_address = dep_graph.root['address']
-    colors: Dict[int, Tuple[float, float, float]] = {
+    colors: dict[int, tuple[float, float, float]] = {
         root_address: (0, 0, 1)
     }
 
-    def _find_colors(tree: Tree, hue_range: Tuple[float, float] = (0, 1), sat: float = 0.5, decline: float = 1):
+    def _find_colors(tree: Tree, hue_range: tuple[float, float] = (0, 1), sat: float = 0.5, decline: float = 1):
         left, right = hue_range
         interval = float(right - left) / len(tree)
         for n, c in enumerate(tree):
