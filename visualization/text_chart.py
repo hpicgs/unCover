301041765afce68b71836caaa94d7ede49fbba5f
Jannis Baum <jannis.baum@gmail.com>
2023-03-09 11:41:37 -0500

    feat(#25): loop through colors & styles

--- visualization/text_chart.py
+++ visualization/text_chart.py
@@ -1,14 +1,25 @@
 # pyright: reportWildcardImportFromLibrary=false
 from dominate.tags import *
 
+# from https://colorbrewer2.org/?type=qualitative&scheme=Set3&n=12)
+colors = ['#8dd3c7','#ffffb3','#bebada','#fb8072','#80b1d3','#fdb462','#b3de69','#fccde5','#d9d9d9','#bc80bd','#ccebc5','#ffed6f']
+# css for bar
+styles = [
+    # solid
+    'background-color: {color}',
+]
+
+# loop through colors for each pattern-style
+# then loop through pattern-styles
+def _get_style(n: int):
+    color = colors[n % len(colors)]
+    return styles[int(n / len(colors)) % len(styles)].format(color=color)
+
 # data: [(sentence/paragraph, [values])]
-# colors: CSS compatible color-strings; min same length as [values] (default
-#     value from https://colorbrewer2.org/?type=qualitative&scheme=Set3&n=12)
 # labels: for [values], has to have same length
 def stacked_bar(
     data: list[tuple[str, list[float]]],
     labels: list[str],
-    colors: list[str] = ['#8dd3c7','#ffffb3','#bebada','#fb8072','#80b1d3','#fdb462','#b3de69','#fccde5','#d9d9d9','#bc80bd','#ccebc5','#ffed6f'],
 ) -> tuple[div, div]:
     stack_height = max(sum(values) for _, values in data)
 
@@ -22,14 +33,14 @@ def stacked_bar(
                     for n, v in enumerate(values):
                         width = 100 * v / stack_height
                         if width > 0:
-                            span(style=f'flex: 0 1 auto; width: {width}%; box-sizing: border-box; background-color: {colors[n]}; border: 1px solid black; {"border-left: 0" if has_left_border else ""}')
+                            span(style=f'flex: 0 1 auto; width: {width}%; box-sizing: border-box; background-color: {_get_style(n)}; border: 1px solid black; {"border-left: 0" if has_left_border else ""}')
                             has_left_border = True
 
     legend = div()
-    for label, color in zip(labels, colors):
+    for n, label in enumerate(labels):
         with legend:
             with p():
-                span(style=f'display: inline-block; width: 1em; height: 1em; background-color: {color}; border: 1px solid black; margin-right: 0.25rem; border-radius: 9999px')
+                span(style=f'display: inline-block; width: 1em; height: 1em; background-color: {_get_style(n)}; border: 1px solid black; margin-right: 0.25rem; border-radius: 9999px')
                 span(label)
 
     return chart, legend
