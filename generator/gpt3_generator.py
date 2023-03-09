dcc8291226c60908a02e8194ab747b3f17fae063
Lucas Liebe <lucasliebe.ll@gmail.com>
2023-03-09 21:14:01 +0100

    feat(#22): add better logging to generator and update README

--- generator/gpt3_generator.py
+++ generator/gpt3_generator.py
@@ -3,11 +3,20 @@ from definitions import GPT_KEY, OPENAI_ORGA
 
 
 def generate_gpt3_news_from_original(doc):
+    print("Starting GPT Request")
     openai.organization = OPENAI_ORGA
     openai.api_key = GPT_KEY
     doc += "\n\nWrite a news article about this topic."
     response = None
     # long docs can throw error but try finally will not work because of long response time so we limit len(doc)
-    if len(doc) < 15000:
-        response = openai.Completion.create(model="text-davinci-003", prompt=doc, max_tokens=800, temperature=0.4)
+    # if len(doc) < 15000:
+    while response is None:
+        try:
+            response = openai.Completion.create(model="text-davinci-003", prompt=doc, max_tokens=800, temperature=0.4)
+        except openai.error.APIConnectionError or openai.error.Timeout:
+            continue
+        except openai.error.InvalidRequestError:
+            break
+
+    print("GPT finished")
     return response.choices[0].text if response else None
