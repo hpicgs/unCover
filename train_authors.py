6e3899df8e93ae1b096d7df821225e9985bf4dbb
tiny-fish-T <van-de-geyern15@gmx.de>
2023-04-12 11:19:29 +0200

    included machine database in training & added a new param

--- train_authors.py
+++ train_authors.py
@@ -1,13 +1,12 @@
 from nltk.parse.corenlp import CoreNLPDependencyParser
 
-from definitions import STYLOMETRY_DIR, DATABASE_AUTHORS_PATH, DATABASE_FILES_PATH
+from definitions import STYLOMETRY_DIR, DATABASE_AUTHORS_PATH, DATABASE_FILES_PATH, DATABASE_MACHINES_PATH
 from stylometry.char_trigrams import char_trigrams
 from stylometry.semantic_trigrams import sem_trigrams
 from stylometry.logistic_regression import trigram_distribution, logistic_regression
-from database.mock_database import DatabaseAuthorship
+from database.mock_database import DatabaseAuthorship, DatabaseMachines
 import os, zipfile, sys, pickle, time, subprocess, argparse
 
-#needed because docker cannot run two bash tasks at the same time
 def preparation():
     server  = subprocess.Popen(["java", "-mx4g", "-cp", "models/stanford-corenlp-4.5.1/*", "edu.stanford.nlp.pipeline.StanfordCoreNLPServer", "-port", "9000", "-timeout", "15000"], stdout=subprocess.PIPE)
     time.sleep(40)
@@ -15,19 +14,27 @@ def preparation():
 
 
 if __name__ == "__main__":
-    #the docker container needs to download this separately
     preparation()
     parser = argparse.ArgumentParser()
     parser.add_argument("--nfeatures", action="store", required=False, type=int, default=100, help="number of char trigram & semantic trigram features used in the distribution")
+    parser.add_argument("--minarticles", action="store", required=False, type=int, default=50, help="minimum number of articles required for training on a specific author/model")
     args = parser.parse_args()
     nfeatures = args.nfeatures
+    minarticles = args.minarticles
     
     if not os.path.isfile(DATABASE_AUTHORS_PATH):
-         print("ERROR: no zipped database was provided")
+         print("ERROR: no database for human authors was provided")
          sys.exit(1)
+    if not os.path.isfile(DATABASE_MACHINES_PATH):
+        print("Error: no database for machine authors was provided")
+        sys.exit(1)
 
     authors = DatabaseAuthorship.get_authors()
+    machines = DatabaseMachines.get_authors()
+    print("Human authors:")
     print(authors)
+    print("Language models:")
+    print(machines)
     training_data = []
     os.makedirs(STYLOMETRY_DIR, exist_ok=True)
 
@@ -35,14 +42,23 @@ if __name__ == "__main__":
     for author in authors:
         full_article_list = [(article["text"], author) for article in DatabaseAuthorship.get_articles_by_author(author)]
         training_data += full_article_list[:int(len(full_article_list)*0.8)]
-        if len(full_article_list) > 50:
+        if len(full_article_list) >= minarticles:
             print("chose author: " + author)
             trainable_authors.append(author)
+    trainable_machines = []
+    for machine in machines:
+        full_article_list = [(article["text"], machine) for article in DatabaseMachines.get_articles_by_author(machine)]
+        training_data += full_article_list[:int(len(full_article_list)*0.8)]
+        if len(full_article_list) >= minarticles:
+            print("chose language model: " + machine)
+            trainable_machines.append(machine)
 
 
     print('number of training articles:', len(training_data))
     print('trainable authors:')
     print(trainable_authors)
+    print('trainable language models:')
+    print(trainable_machines)
 
     char_grams = [char_trigrams(article_tuple[0]) for article_tuple in training_data]
     parser = CoreNLPDependencyParser(url='http://localhost:9000')
@@ -50,12 +66,11 @@ if __name__ == "__main__":
     
     character_distribution = trigram_distribution(char_grams, nfeatures)
     semantic_distribution = trigram_distribution(sem_grams, nfeatures)
-    #print(character_distribution)
-    #print(semantic_distribution)
     character_distribution.to_csv("models/stylometry/char_distribution"  + str(nfeatures) +  ".csv")
     semantic_distribution.to_csv("models/stylometry/sem_distribution"  + str(nfeatures) + ".csv")
 
-    for author in trainable_authors:
+    training_subjects = trainable_authors + trainable_machines
+    for author in trainable_subjects:
         truth_table = [1 if author == article_tuple[1] else 0 for article_tuple in training_data]
         print(truth_table)
         with open(os.path.join(STYLOMETRY_DIR, author.replace('/', '_') + '_char' + str(nfeatures) + '.pickle'), 'wb') as f:
