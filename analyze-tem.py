4400627d69e85c87aeaca6aa4ec49f5bf23d91ae
Jannis Baum <jannis.baum@gmail.com>
2023-04-12 16:46:54 +0200

    feat(#28): label graph png

--- analyze-tem.py
+++ analyze-tem.py
@@ -17,3 +17,32 @@ def te_analysis_data(te: TopicEvolution) -> dict[str, float]:
         'largest group / n_nodes': max((count for count in node_count_by_id.values())) / node_count,
         'mean n_words per topic': statistics.mean([len(words) for period in te.periods for topic in period.topics for words in topic.words])
     }
+
+def te_analysis_img(text: str) -> bytes:
+    corpus = [docs_from_period(line) for line in text.split('\n') if len(line) > 0]
+    corpus = merge_short_periods(corpus, min_docs=2)
+    te = get_topic_evolution(
+        corpus,
+        c=0.5,
+        alpha=0,
+        beta=-1,
+        gamma=0,
+        delta=1,
+        theta=2,
+        mergeThreshold=100,
+        evolutionThreshold=100
+    )
+
+    graph = te.graph()
+    graph.attr(label='''<<FONT POINT-SIZE="48" COLOR="white">
+        <TABLE BGCOLOR="red" ALIGN="left" BORDER="0" CELLBORDER="0" CELLSPACING="40">
+            {rows}
+        </TABLE>
+    </FONT>>'''.format(rows='\n'.join(['''
+        <TR>
+            <TD ALIGN="left">{key}</TD>
+            <TD ALIGN="right">{value}</TD>
+        </TR>
+    '''.format(key=key, value=value) for key, value in te_analysis_data(te).items()])))
+
+    return graph.pipe(format='png')
