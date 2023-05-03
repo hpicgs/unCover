9c296b15f6f5bd8fbe0b26f7f9a9fd2b3bbce639
Jannis Baum <jannis.baum@gmail.com>
2023-05-03 14:58:55 +0200

    feat: get default te from text

--- tem/process.py
+++ tem/process.py
@@ -2,6 +2,7 @@ import subprocess
 import yaml
 
 from tem.model import TopicEvolution
+from tem.nlp import docs_from_period, merge_short_periods
 
 def get_topic_evolution(
     corpus: list[list[list[str]]], # periods, docs, words
@@ -35,3 +36,18 @@ def get_topic_evolution(
     out = p.communicate(input=structured_text.encode())[0].decode()
 
     return TopicEvolution(yaml.safe_load(out))
+
+def get_default_te(text: str) -> TopicEvolution:
+    corpus = [docs_from_period(line) for line in text.split('\n') if len(line) > 0]
+    corpus = merge_short_periods(corpus, min_docs=2)
+    return get_topic_evolution(
+        corpus,
+        c=0.5,
+        alpha=0,
+        beta=-1,
+        gamma=0,
+        delta=1,
+        theta=2,
+        mergeThreshold=100,
+        evolutionThreshold=100
+    )
