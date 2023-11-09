86b265e58f964b7ab70d4a90fc473fcced1c251d
Lucas Liebe <lucasliebe.ll@gmail.com>
2023-11-09 16:29:20 +0900

    fix(#9): remove streamlit secrets and adjust install.sh

--- install.sh
+++ install.sh
@@ -4,13 +4,18 @@ print_bold() {
     printf "\\033[1m$1\\033[0m\n"
 }
 
-if ! which java 1>/dev/null 2>/dev/null; then
-    print_bold "Please ensure Java is installed and on your PATH."
-    exit 1
-fi
 
 if ! which conda 1>/dev/null 2>/dev/null; then
     print_bold "Please ensure Anaconda is installed and on your PATH."
+    exit 1
+fi
+
+print_bold "Creating Anaconda environment"
+conda env create -f environment.yml
+conda activate unCover
+
+if ! which java 1>/dev/null 2>/dev/null; then
+    print_bold "Please ensure Java is installed and on your PATH."
     exit 1
 fi
 
@@ -26,15 +31,10 @@ cd unCover
 print_bold "Compiling TEM"
 make -C tem/topic-evolution-model/
 
-print_bold "Creating Anaconda environment"
-conda env create -f environment.yml
-conda activate unCover
-
 print_bold "Installing CoreNLP"
 ./corenlp --no-run
 
 print_bold "Downloading Models"
 ./prepare_models
-cp ./.env.example ./.env
 
 print_bold "Done!"
