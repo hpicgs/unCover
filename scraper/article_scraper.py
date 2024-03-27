91b5a0ccd04e952dcaec7e9c1002c63dde6ea670
Lucas Liebe <lucasliebe.ll@gmail.com>
2024-03-27 21:30:39 +0900

    feat(#52): adjust test dataset generation to german

--- scraper/article_scraper.py
+++ scraper/article_scraper.py
@@ -21,41 +21,12 @@ class GoogleScraper:
         if self.__verbose:
             print(f"\r\033[K{self.__log_prefix}{message}", end="\n" if last else "")
 
-    def find_urls_for_query(self, query):
-        self.__driver.get("https://google.com")
-        try:
-            agree_btn = next(
-                btn
-                for btn in self.__driver.find_elements(
-                    By.TAG_NAME, "button"
-                )
-                if "Alle akzeptieren" in btn.get_attribute("innerHTML")
-            )
-            agree_btn.click()
-        except:
-            pass
-        search_bar = self.__driver.find_element(
-            By.XPATH, '//input[@title="Suche"][@name="q"]'
-        )
-        search_bar.send_keys(query + Keys.ENTER)
-
-        fetch_results = lambda: [
-            a_tag.get_attribute("href")
-            for a_tag in self.__driver.find_elements(By.XPATH, "//a/h3/..")
-        ]
-        results = list()
-        while not results:
-            results = fetch_results()
-        results_prev = list()
-        while len(results_prev) < len(results):
-            results_prev = results
-            results = fetch_results()
-            time.sleep(1)
-        return results
-
-    def find_news_urls_for_query(self, query, n_articles=10, site=None):
+    def find_news_urls_for_query(self, query, n_articles=10, german=False):
         self.__log(f"scraping for query {query}")
-        self.__driver.get("https://news.google.com")
+        if not german:
+            self.__driver.get("https://news.google.com")
+        else:
+            self.__driver.get("https://news.google.de")
         self.__log(f"received news.google.com page")
         time.sleep(3)
         try:
@@ -64,7 +35,8 @@ class GoogleScraper:
                 for btn in self.__driver.find_elements(
                     By.TAG_NAME, "button"
                 )
-                if "Accept all" in btn.get_attribute("innerHTML")
+                if "Accept all" in btn.get_attribute("innerHTML") or
+                "Alle akzeptieren" in btn.get_attribute("innerHTML")
             )
             agree_btn.click()
             self.__log("agreed to google's data collection")
@@ -72,29 +44,31 @@ class GoogleScraper:
             pass
 
         time.sleep(3)
-        search_bar = self.__driver.find_element(
-            By.XPATH, '//input[@value="Search for topics, locations & sources"]'
-        )
+        if not german:
+            search_bar = self.__driver.find_element(
+                By.XPATH, '//input[@value="Search for topics, locations & sources"]'
+            )
+        else:
+            search_bar = self.__driver.find_element(
+                By.XPATH, '//input[@value="Themen, Orte und Quellen suchen"]'
+            )
         action = webdriver.common.action_chains.ActionChains(
             self.__driver
         )
         action.move_to_element_with_offset(search_bar, 5, 5)
         action.click()
-        if site:
-            action.send_keys(f"{query} site:{site}" + Keys.ENTER)
-        else:
-            action.send_keys(query + Keys.ENTER)
+        action.send_keys(query + Keys.ENTER)
         action.perform()
         self.__log("executed search, fetching results now")
 
         time.sleep(3)
         fetch_results = lambda: [
-            a_tag.get_attribute("href")
-            for a_tag in self.__driver.find_elements(
+                                    a_tag.get_attribute("href")
+                                    for a_tag in self.__driver.find_elements(
                 By.XPATH, "//article/div/div/a"
             )
-            if a_tag.is_displayed()
-        ][:n_articles]
+                                    if a_tag.is_displayed()
+                                ][:n_articles]
 
         time.sleep(10)
         results = fetch_results()
@@ -112,11 +86,10 @@ class GoogleScraper:
         for url in results:
             self.__driver.get(url)
             while (
-                self.__driver.current_url == url
-                or self.__driver.current_url == "about:blank"
+                    self.__driver.current_url == url
+                    or self.__driver.current_url == "about:blank"
             ):
                 time.sleep(0.2)
             urls.append(self.__driver.current_url)
         self.__log(f"{len(urls)} news urls extracted", last=True)
         return urls
-
